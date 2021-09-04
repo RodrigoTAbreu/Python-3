913487d102062c12fff91cda1b8ce15fe8fbf83a
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-04 06:01:42 -0300

    Aula13

--- 100ExercíciosPython/Mundo2/54-Palindromo.py
+++ 100ExercíciosPython/Mundo2/54-Palindromo.py
@@ -1,2 +1,14 @@
-frase = str(input('Digite a frase:')).strip().upper()
-print(frase)
+frase = str(input('Digite a frase:')).strip().upper() #.strip() elimina os espaços / .upper() coloca tudo em maiusculas
+palavras = frase.split() #quebrando a frase palavra a palavra
+junto = ''.join(palavras)# juntando as palavras sem espaço
+inverso = ''
+inverso = junto[::-1] # pode-se utilizar esse macete de fatiamento, dessa forma elimina o FOR
+'''for letra in range(len(junto)-1, -1, -1): #para cada letra na range do TAMANHO de JUNTO até o final(-1), até a ultima letra (-1), contando de regressivo (-1)
+    inverso += junto[letra] #somando a variavel inverso a variavel JUNTO na posição LETRA
+'''
+if inverso == junto: # verificando se ambas são iguais
+    print('É um palíndromo!!')
+else:
+    print('Não é palindoromo.')
+
+print(junto, inverso)
