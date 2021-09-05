7c054023964058643015b0b5fb5cee473160a0be
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-05 03:38:01 -0300

    Aula14

--- 100ExercíciosPython/Mundo2/57-ValidaçãoDeDados.py
+++ 100ExercíciosPython/Mundo2/57-ValidaçãoDeDados.py
@@ -1,4 +1,6 @@
-sexo = str(input('Informe o sexo: '))
-while sexo != 'M' or 'F':
-    sexo = str(input('Dados inválidos. Informe seu Sexo[M/F]:'))
-print('fim')
+sexo = str(input('Informe o sexo: ')).upper()
+cont = 0
+while sexo != 'M' and sexo !='F':
+    sexo = str(input('Dados inválidos. Informe seu Sexo[M/F]:')).upper()
+    cont +=1
+print('Foram necessárias {} tentativas.'.format(cont))
