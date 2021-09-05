170d4bb61a97df43c880ee9eead3bb032eede162
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-05 05:05:49 -0300

    Aula14

--- 100ExercíciosPython/Mundo2/58-JogoAdivinhação..py
+++ 100ExercíciosPython/Mundo2/58-JogoAdivinhação..py
@@ -3,19 +3,22 @@ print('-='*10,end='')
 print(' Jogo da Adivinhação ',end='')
 print('=-'*10)
 print('Vamos, jogar. Eu escolhi um número de 0 até 10. Qual é?')
-computador = randint(0,10)
+computador = randint(0,10) #computador vai escolher um número de 0 a 10
 #print(computador)
-player = int(input('Digite seu palpite: '))
+player = int(input('Digite seu palpite: ')) #jogador digita um número
 #print(player)
-cont = 0
-
-while player != computador:
-    cont += 1
-    print('Você errou tente novamente.')
-    player = int(input('Digite um número para adivinhar: '))
-
-    if player > computador:
-        print('Menos..')
-    elif player < computador:
-        print(('Mais..'))
+cont = 0 # contador dos palpites ou chances
+acertou = False #variavel que é false até que ele acerte e mude para TRUE
+while not acertou: # enquanto não acertou
+    cont += 1 #contagem dos palpites se inicia
+    if cont == 1:
+        print('Tá dificil ??!!! Vou te ajudar. ')
+    player = int(input('Digite um número para adivinhar: ')) #jogo solicita a entrada de mais uma tentativa.
+    if player == computador: #se o jogador digitar um número igual ao do computador
+        acertou = True # muda a variável para TRUE
+    else:        # se for diferente o jogo vai dar algumas dicas
+        if player > computador: #se o jogador escolher um numero maior que o do computador
+            print('Menos..')
+        elif player < computador: #se o jogador escolher um numero menor que o do computador
+            print(('Mais..'))
 print('Você acertou com {} tentativas. Parabéns!!'.format(cont))
