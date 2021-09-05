28159702de2c706304163aaed54a97f3d688e8a4
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-05 03:43:00 -0300

    Aula14

--- 100ExercíciosPython/Mundo2/57-ValidaçãoDeDados.py
+++ 100ExercíciosPython/Mundo2/57-ValidaçãoDeDados.py
@@ -1,6 +1,8 @@
-sexo = str(input('Informe o sexo: ')).upper()
+sexo = str(input('Informe o sexo: ')).upper().strip()[0] # com [0] ele pega somente a primeira letra
 cont = 0
+#outra forma do WHILE
+# while sexo not in 'MmFf':
 while sexo != 'M' and sexo !='F':
-    sexo = str(input('Dados inválidos. Informe seu Sexo[M/F]:')).upper()
+    sexo = str(input('Dados inválidos. Informe seu Sexo[M/F]:')).upper().strip()[0]
     cont +=1
 print('Foram necessárias {} tentativas.'.format(cont))
