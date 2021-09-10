1c7a1220677e54f2daab74fe099dce73d11ac0cd
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-10 03:05:56 -0300

    Aula16

---
+++ 100ExercíciosPython/Mundo3/74-MaiorMenorValorDaTupla.py
@@ -0,0 +1,9 @@
+from random import randint
+n = (randint(1,10),randint(1,10),randint(1,10),randint(1,10),
+     randint(1,10))
+
+
+print(f'Os números sorteados foram: {n}')
+print(f'O maior número é: {max(n)}.')
+print(f'O menor número gerado é: {min(n)}')
+
