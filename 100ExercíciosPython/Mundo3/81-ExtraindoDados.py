b7e3aae413e73e897c39b30069c07580a9da8a73
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-11 05:24:59 -0300

    Aula17

---
+++ 100ExercíciosPython/Mundo3/81-ExtraindoDados.py
@@ -0,0 +1,17 @@
+lista = []
+
+while True:
+    n = int(input('Digite um número: '))
+    lista.append(n)
+    resp = input('Quer continuar? [S/N]: ').upper()[0]
+
+    if resp == 'N':
+        break
+
+if 5 in lista:
+    print('O 5 faz parte da lista')
+
+print(lista)
+print(f'Foram digitados {len(lista)} numeros na lista.')
+lista.sort(reverse=True)
+print(f'Lista em orde decrescente {lista}.')
