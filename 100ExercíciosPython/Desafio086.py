2bce9f0b4e972297fd6cdee58595ad1527ab1db5
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-12 04:02:51 -0300

    Aula18

--- 100ExercíciosPython/Desafio086.py
+++ 100ExercíciosPython/Desafio086.py
@@ -1,9 +1,11 @@
-matriz = [[0,0,0],[0,0,0],[0,0,0]]
-for l in range(0,3):
-    for c in range(0,3):
+matriz = [[0,0,0],[0,0,0],[0,0,0]] # lista com as posições definidas
+for l in range(0,3): # para linha de 0 a 3
+    for c in range(0,3): # para a coluna de 0 a 3
+# adiciona os valores na lista e nas posições já predefinidas LINHA E COLUNA
         matriz[l][c] = int(input(f'Digite o valor para [{l},{c}]: '))
 print('=='*30)
+# para cada linha de 0 a 3
 for l in range(0,3):
-    for c in range(0,3):
-        print(f'[{matriz[l][c]:^5}]', end=' ')
-    print()
+    for c in range(0,3):#para cada coluna de 0 a 3
+        print(f'[{matriz[l][c]:^5}]', end=' ') #imprime a lista na posição L e C
+     print()
