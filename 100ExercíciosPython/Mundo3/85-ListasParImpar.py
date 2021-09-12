1f74f04b4478ea5e121f81838927758bb33dcfc0
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-12 03:56:50 -0300

    Aula18

--- 100ExercíciosPython/Mundo3/85-ListasParImpar.py
+++ 100ExercíciosPython/Mundo3/85-ListasParImpar.py
@@ -1,12 +1,14 @@
-lista = [[],[]]
+lista = [[],[]] # cria a lista com as posição específica.
 n = 0
 
 for c in range(1,8):
     n = int(input(f'Digite o {c}° um número: '))
-    if n %2==0:
-        lista[0].append(n)
+    if n %2 == 0:
+        lista[0].append(n) #adicionando na posição 0
     else:
-        lista[1].append(n)
+        lista[1].append(n) #adicionando na posição 1
 
+lista[0].sort() # coloca em ordem os itens da posição 0
+lista[1].sort() # coloca em ordem os itens da posição 1
 print(f'Os pares são {lista[0]}.')
 print(f'Os ímpares são {lista[1]}.')
