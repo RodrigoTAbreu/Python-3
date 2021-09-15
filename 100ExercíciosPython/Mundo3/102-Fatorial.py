4e88247bf3c849173876f6e2041c07e3855ed627
RodrigoTAbreu <rodrigo.telles.abreu@gmail.com>
2021-09-15 10:53:00 -0300

    Aula19

---
+++ 100ExercíciosPython/Mundo3/102-Fatorial.py
@@ -0,0 +1,8 @@
+def fatorial(a=0,show=True):
+    '''
+    --> Calcula o Fatorial de um número:
+    :param n: O número  a ser calculado.
+    :param show: (opcional) Mostrar ou não a conta.
+    :return: O valor do Fatorial de um número n.
+    '''
+
