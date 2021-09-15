781c3bc26fbc471822fed12f33664bba29a8bd11
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-15 02:44:17 -0300

    Aula19

---
+++ 100ExercíciosPython/Mundo3/97-PrintEspecial.py
@@ -0,0 +1,10 @@
+def escreva(txt):
+    print('-'*len(txt))
+    print(txt)
+    print('-' * len(txt))
+
+
+#Programa principal.
+escreva('Olá, Mundo')
+escreva('Aprendendo PYTHON')
+escreva('Eu vou ser um programador.')
