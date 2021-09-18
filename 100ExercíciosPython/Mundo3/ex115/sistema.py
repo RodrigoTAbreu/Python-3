3ec8cf6dcaf6b054d491cf8d1bcbf5fd4f014a50
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-18 06:30:26 -0300

    Aula23

--- 100ExercíciosPython/Mundo3/ex115/sistema.py
+++ 100ExercíciosPython/Mundo3/ex115/sistema.py
@@ -1,9 +1,18 @@
 from lib import interface
 from time import sleep
+from lib import arquivo
+
+arq = 'arquivo1.txt'
+if not arquivo.arquivoExiste(arq):
+    arquivo.criarArquivo(arq)
+
 while True:
+    arq = open('arquivo1.txt','w')
     resposta = interface.menu(['Consulta de Pessoas','Cadastro de Pessoas','Sair do Sistema'])
     if resposta == 1:
-        interface.cabecalho('\033[34mOpção 1\033[m')
+        interface.cabecalho('\033[34mConsulta de Pessoas\033[m')
+        arquivo.lerArquivo(arq)
+        sleep(2)
     elif resposta == 2:
         interface.cabecalho('\033[34mOpção 2\033[m')
     elif resposta == 3:
