920c7002c5414b8283a352c1bcfe1aee2c81de3f
Rodrigo Abreu <rodrigo.telles.abreu@gmail.com>
2021-09-19 04:50:59 -0300

    Aula23

--- 100ExercíciosPython/Mundo3/ex115/sistema.py
+++ 100ExercíciosPython/Mundo3/ex115/sistema.py
@@ -2,19 +2,24 @@ from lib import interface
 from time import sleep
 from lib import arquivo
 
-arq = 'arquivo1.txt'
+arq = 'arq.txt'
 if not arquivo.arquivoExiste(arq):
     arquivo.criarArquivo(arq)
 
 while True:
-    arq = open('arquivo1.txt','w')
+    arq = open('arq.txt','rt')
     resposta = interface.menu(['Consulta de Pessoas','Cadastro de Pessoas','Sair do Sistema'])
     if resposta == 1:
         interface.cabecalho('\033[34mConsulta de Pessoas\033[m')
-        arquivo.lerArquivo(arq)
+        arquivo.lerarquivo(arq.name)
         sleep(2)
     elif resposta == 2:
-        interface.cabecalho('\033[34mOpção 2\033[m')
+        #interface.cabecalho('\033[34mOpção 2\033[m')
+        interface.cabecalho('NOVO CADASTRO')
+        nome = str(input('Nome: '))
+        idade = interface.leiaint('Idade: ')
+        arquivo.cadastrar(arq,nome,idade)
+
     elif resposta == 3:
         interface.cabecalho('\033[34mOpção 3\033[m')
         break
---
+++ 100ExercíciosPython/Mundo3/teste.py
@@ -0,0 +1,4 @@
+num = (2,8,4,7)
+print(sorted(num))
+print(num)
+print(num[0], num[-1])
